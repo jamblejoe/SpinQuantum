47a56e25e56c929ec9f0813c907a6d974fa29e94
Goran Nakerst <go.nakerst@web.de>
2021-11-01 19:32:31 +0000

    refactored general operator creation and added tests

--- test/basis_tests.jl
+++ test/basis_tests.jl
@@ -1,5 +1,28 @@
 @testset "TensorBasis" begin
-    for L in 1:4
+
+    @testset "L=2" begin
+        
+        basis = TensorBasis(2)
+        @test getstate(basis, 1) == [0,0]
+        @test getstate(basis, 2) == [0,1]
+        @test getstate(basis, 3) == [1,0]
+        @test getstate(basis, 4) == [1,1]
+    end
+
+    @testset "L=3" begin
+        
+        basis = TensorBasis(3)
+        @test getstate(basis, 1) == [0,0,0]
+        @test getstate(basis, 2) == [0,0,1]
+        @test getstate(basis, 3) == [0,1,0]
+        @test getstate(basis, 4) == [0,1,1]
+        @test getstate(basis, 5) == [1,0,0]
+        @test getstate(basis, 6) == [1,0,1]
+        @test getstate(basis, 7) == [1,1,0]
+        @test getstate(basis, 8) == [1,1,1]
+    end
+
+    for L in 1:6
         basis = TensorBasis(L)
         for i in eachindex(basis)
             @test getposition(basis, getstate(basis, i)) == i
